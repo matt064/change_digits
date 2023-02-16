3643a45957f3f1ad9387a2bd9ddf773ab41eb49c
Mateusz Gawroński <gawronski.m91@gmail.com>
2023-02-16 17:47:48 +0100

    test final number is integer

--- tests/test_roman.py
+++ tests/test_roman.py
@@ -18,3 +18,8 @@ def test_simple_roman_to_integer_number():
 def test_complex_roman_to_integer_number():
     wynik = convert_digit('MCMXCIII')
     assert wynik == 1993
+
+
+def test_final_number_is_integer():
+    wynik = convert_digit("VII")
+    assert type(wynik) == int
