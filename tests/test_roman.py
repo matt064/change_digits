7d7d444d77d3a2fb07c5ed38876fbf70fa0b086c
Mateusz Gawroński <gawronski.m91@gmail.com>
2023-02-16 10:51:24 +0100

    test variable is none

--- tests/test_roman.py
+++ tests/test_roman.py
@@ -1,3 +1,6 @@
+from roman.roman import convert_digit
+
+
 def test_import_from_roman():
     try:
         from roman.roman import convert_digit
@@ -5,3 +8,7 @@ def test_import_from_roman():
     except ImportError as error:
         assert False, error
 
+
+def test_variable_is_none():
+    assert convert_digit(" ") is None
+    
